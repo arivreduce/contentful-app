4b41ad4d2eb5e595a40af07cbb24a3f9fff8e935
Aaron Rivas <aarivas793@gmail.com>
2024-09-26 15:42:15 -0700

    initial commit

---
+++ src/Hero.jsx
@@ -0,0 +1,22 @@
+import heroImg from './assets/hero.svg';
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <div className="hero-title">
+          <h1>Contentful CMS</h1>
+          <p>
+            Lorem ipsum dolor, sit amet consectetur adipisicing elit. Labore
+            odit exercitationem id expedita rem doloremque porro illo? Numquam
+            facere non corporis consequatur! Vero, ut earum!
+          </p>
+        </div>
+        <div className="img-container">
+          <img src={heroImg} alt="woman and the browser" className="img" />
+        </div>
+      </div>
+    </section>
+  );
+};
+export default Hero;
